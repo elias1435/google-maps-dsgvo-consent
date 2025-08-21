2fd99fae2bf3101ec38bde4e98e59ba3333c314c
Md. Elias <elias1435@gmail.com>
2025-08-21 12:51:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById('gm-btn').addEventListener('click', function () {
 	const placeholder = this.closest('.googlemaps-placeholder');
 	placeholder.outerHTML =
 		'<div class="googlemaps-responsive">' +
-		'<iframe src="https://www.google.com/maps/embed?..."></iframe>' +
+		'<iframe src="https://www.google.com/maps/embed?..." width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'' +
 		'</div>';
 });
